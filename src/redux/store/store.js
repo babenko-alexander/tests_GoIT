72b57fa9b338305879960c97f8f917f79942ea1b
Alexandr-fly-code <alexandr17world@gmail.com>
2018-08-20 21:17:04 +0300

    fixed store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReduser from './index';
+import rootReduser from '../reducers';
 
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
