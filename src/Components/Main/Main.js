9509da6751dd352e452b924914e70a0c7317ab90
Dmitro Didenko <dmitrdida@ukr.net>
2018-08-21 16:03:18 +0300

    done main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './Main.css'
+import {NavLink} from 'react-router-dom';
+import styles from './Main.css';
 
 const Main = () => {
     return (
@@ -12,36 +13,76 @@ const Main = () => {
                     <div className={styles.main__box}>
                         <div className={`${styles.box__task} ${styles.css_box}`}>
                             <p>HTML, CSS</p>
-                            <span className={`${styles.main__span} ${styles.mod_1}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_2}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_3}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_4}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_7}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_8}`}>lorem</span>
+                            <NavLink exact to='/'>
+                                <span className={`${styles.main__span} ${styles.mod_1}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/2'>
+                                <span className={`${styles.main__span} ${styles.mod_2}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/3'>
+                                <span className={`${styles.main__span} ${styles.mod_3}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/4'>
+                                <span className={`${styles.main__span} ${styles.mod_4}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/5'>
+                                <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/6'>
+                                <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/7'>
+                                <span className={`${styles.main__span} ${styles.mod_7}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/8'>
+                                <span className={`${styles.main__span} ${styles.mod_8}`}>lorem</span>
+                            </NavLink>
                         </div>
                     </div>
                     <div className={styles.main__box}>
                         <div className={styles.box__task}>
                             <p>Satellite</p>
-                            <span className={`${styles.main__span} ${styles.modul_1}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_2}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_3}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_4}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
+                            <NavLink to='/9'>
+                                <span className={`${styles.main__span} ${styles.modul_1}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/10'>
+                                <span className={`${styles.main__span} ${styles.modul_2}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/11'>
+                                <span className={`${styles.main__span} ${styles.modul_3}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/12'>
+                                <span className={`${styles.main__span} ${styles.modul_4}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/13'>
+                                <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/14'>
+                                <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
+                            </NavLink>
                         </div>
                     </div>
                     <div className={styles.main__box}>
                         <div className={`${styles.box__task} ${styles.java_box}`}>
                             <p>Java Script</p>
-                            <span className={`${styles.main__span} ${styles.modul_1}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_2}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_3}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.modul_4}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
-                            <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
+                            <NavLink to='/15'>
+                                <span className={`${styles.main__span} ${styles.modul_1}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/16'>
+                                <span className={`${styles.main__span} ${styles.modul_2}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/17'>
+                                <span className={`${styles.main__span} ${styles.modul_3}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/18'>
+                                <span className={`${styles.main__span} ${styles.modul_4}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/19'>
+                                <span className={`${styles.main__span} ${styles.mod_5}`}>lorem</span>
+                            </NavLink>
+                            <NavLink to='/20'>
+                                <span className={`${styles.main__span} ${styles.mod_6}`}>lorem</span>
+                            </NavLink>
                         </div>
                     </div>
                 </div>
@@ -49,6 +90,5 @@ const Main = () => {
         </div>
     );
 };
-
 
 export default Main;
