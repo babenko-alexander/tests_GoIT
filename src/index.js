3fe470785de9cd5832342957721f0ca76b2d6014
Dmitro Didenko <dmitrdida@ukr.net>
2018-08-21 11:14:32 +0300

    new style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/reducers/store';
-import './index.css';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
-    <Provider store = {store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
--- src/redux/Main/Main.js
+++ src/redux/Main/Main.js
@@ -8,18 +8,35 @@ const Main = () => {
                 <h1 className="title">Проверь свои знания Front End</h1>
                 <p className="sub-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto,
                     consequatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, rerum! </p>
-                <div className="main__section">
-                    <div className="section__box">
-                        <div className="task">HTML, CSS</div>
-                        <span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span>
+                <div className="section__main">
+                    <div className="main__box">
+                        <div className="box__task">HTML, CSS</div>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
                     </div>
-                    <div className="section__box">
-                        <div className="task">Satellite</div>
-                        <span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span>
+                    <div className="main__box">
+                        <div className="box__task">Satellite</div>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
                     </div>
-                    <div className="section__box">
-                        <div className="task">Java Script</div>
-                        <span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span><span>lorem</span>
+                    <div className="main__box">
+                        <div className="box__task">Java Script</div>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
+                        <span className="main__span">lorem</span>
                     </div>
                 </div>
             </section>
