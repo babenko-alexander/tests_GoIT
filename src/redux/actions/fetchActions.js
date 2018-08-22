7608fb3b639c9b4fd357d95b00729fe3a4b161c1
Dmitro Didenko <dmitrdida@ukr.net>
2018-08-22 16:52:13 +0300

    new

--- src/redux/actions/fetchActions.js
+++ src/redux/actions/fetchActions.js
@@ -11,7 +11,7 @@ function fetchAllTests() {
     return axios.get('http://localhost:3001/tests'
     // .then(result => {console.log(result.data[0]); return result})  // test
         .then(result => result.status === 200 ? result : null)
-        .catch(err => console.log(err))
+        .catch(err => console.log(err)))
 }
 
 export const fetchAllTestsDataAsync = () => dispatch => {
