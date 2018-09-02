e0527033994bb49641410809b6dc640e62e046ae
Sergey Plaski <splaski@gmail.com>
2018-09-02 19:14:29 +0300

    merged in merge_branch and Main, fixed errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import Main from './Components/Main/Main';
+
 import {fetchAllTestsDataAsync} from './redux/actions/fetchActions';
-// import {getSingleTestAsync} from './redux/actions/testActions';
 import {addCurrentCorrectResult} from './redux/actions/currentCorrectResultActions';
 import Test from './Components/Test/Test';
 
@@ -12,15 +13,19 @@ class App extends Component {
 
     componentDidMount() {
         this.props.loadAllTestsDataAsync();
-        // this.props.loadSingleTestAsync('HTML, CSS', 'Медиа запросы');
-        let correctAnswerData = this.props.selectedTest.questions.map(el => el.rightAnswer);
-        this.props.addCurrentCorrectResult(correctAnswerData)
     };
 
-    render() {
-        return (
+     render() {
+         const testIsReady = Object.keys(this.props.selectedTest).length > 0;
+         // console.log('testIsReady: ', testIsReady);
+         if (testIsReady) {
+             let correctAnswerData = this.props.selectedTest.questions.map(el => el.rightAnswer);
+             this.props.addCurrentCorrectResult(correctAnswerData)
+         }
+
+         return (
             <div className="App">
-                <Main/>
+                {testIsReady ? <Test/> : <Main/>}
             </div>
         );
     }
@@ -38,9 +43,6 @@ function MDTP(dispatch) {
         loadAllTestsDataAsync: function() {
             dispatch(fetchAllTestsDataAsync())
         },
-        // loadSingleTestAsync: function(module, testname) {
-        //   dispatch(getSingleTestAsync(module, testname))
-        // },
         addCurrentCorrectResult: function (data) {
             dispatch(addCurrentCorrectResult(data))
         }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,49 +1,59 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import styles from './Main.css';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
 
-const Main = ({tests}) => {
+import {getSingleTestAsync} from '../../redux/actions/testActions';
+
+import styles from './Main.css';
+
+const Main = ({tests, loadSingleTestAsync}) => {
+
+    const selectTest = function(e) {
+        console.log(e.target.dataset.module, e.target.dataset.testname);
+        loadSingleTestAsync(e.target.dataset.module, e.target.dataset.testname);
+    };
+
     const taskOne = [styles.mod_1, styles.mod_2, styles.mod_3, styles.mod_4, styles.mod_5, styles.mod_6, styles.mod_7, styles.mod_8];
     const taskTwo = [styles.mod1, styles.mod2, styles.mod3, styles.mod4, styles.mod_5, styles.mod_6];
-    return (
-        <div className={styles.container}>
-            <section className={styles.section}>
-                <h1 className={styles.title}>Проверь свои знания Front End</h1>
-                <p className={styles.sub__title}>Здраствуйте дорогие студенты, надеемся что, тесты Go It не только
-                    принесут вам пользу и знания, но и множество эмоций, и удовольствия от их прохождения</p>
-                <div className={styles.section__main}>
-                    <div className={styles.main__box}>
-                        <div className={`${styles.box__task} ${styles.css_box}`}>
-                            <p>HTML, CSS</p>
-                            {tests.length ? tests[0]['module-tests'].map((el, ind) =>
-                                <NavLink to={'/' + `${ind}`} key={el["test-name"] + ind}>
-                                    <span className={`${styles.main__span} ${taskOne[ind]}`}>{el["test-name"]}</span>
-                                </NavLink>) : null}
-                        </div>
+    const moduleCircle = [styles.box__task_0, styles.box__task_1, null];
+
+    if (tests.length) {
+        // const modules = tests;
+        const modules = tests.sort((a,b) => a.module > b.module);
+        // console.log('modules:', modules);
+
+        return (
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <h1 className={styles.title}>Проверь свои знания Front End</h1>
+                    <p className={styles.sub__title}>Здраствуйте дорогие студенты, надеемся что, тесты GoIT не только
+                        принесут вам пользу и знания, но и множество эмоций, и удовольствия от их прохождения</p>
+
+                    <div className={styles.section__main}>
+                        {modules.map((m, index) =>
+                            <div className={styles.main__box} key={index}>
+                                <div className={`${styles.box__task} ${moduleCircle[index]}`}>
+                                    <p>{m.module}</p>
+                                    {m['module-tests'].map((t, ind) =>
+                                        <span
+                                            className={`${styles.main__span} ${modules.indexOf(m) === 0 ? taskOne[ind] : taskTwo[ind]}`}
+                                            key={`${m.module}${ind}`}
+                                            data-module={m.module}
+                                            data-testname={t['test-name']}
+                                            onClick={selectTest}
+                                        >
+                                        {t["test-name"]}
+                                    </span>)
+                                    }
+                                </div>
+                            </div>)
+                        }
                     </div>
-                    <div className={styles.main__box}>
-                        <div className={styles.box__task}>
-                            <p>Satellite</p>
-                            {tests.length ? tests[1]['module-tests'].map((el, ind) =>
-                                <NavLink to={'/' + `${ind}`} key={el["test-name"] + ind}>
-                                    <span className={`${styles.main__span} ${taskTwo[ind]}`}>{el["test-name"]}</span>
-                                </NavLink>) : null}
-                        </div>
-                    </div>
-                    <div className={styles.main__box}>
-                        <div className={`${styles.box__task} ${styles.java_box}`}>
-                            <p>Java Script</p>
-                            {tests.length ? tests[2]['module-tests'].map((el, ind) =>
-                                <NavLink to={'/' + `${ind}`} key={el["test-name"] + ind}>
-                                    <span className={`${styles.main__span} ${taskTwo[ind]}`}>{el["test-name"]}</span>
-                                </NavLink>) : null}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
+                </section>
+            </div>
+        );
+    } else {
+        return null;
+    }
 };
 
 function MSTP(state) {
@@ -53,7 +63,15 @@ function MSTP(state) {
     }
 }
 
-export default connect(MSTP, null)(Main);
+function MDTP(dispatch) {
+    return {
+        loadSingleTestAsync: function(module, testname) {
+            dispatch(getSingleTestAsync(module, testname))
+        }
+    }
+}
+
+export default connect(MSTP, MDTP)(Main);
 
 
 
