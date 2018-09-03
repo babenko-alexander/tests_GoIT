0635e545105c3ba8e83c5590aaf0a70d5c86f6c5
Yeuhen <lyunkor@gmail.com>
2018-09-03 11:32:46 +0300

    Header_html+css_descktop_ver

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,21 +11,26 @@ const Header = (props) => {
     return (
         <div className={styles.header__container}>
 
-            <a href="/" className={styles.header__logo}>
-                <NavLink exact to='/'><img src={logo} alt="logo"/>FunTest</NavLink></a>
+        <NavLink exact to='/' className={styles['header__main-nav-link']}>
+            <img src={logo} alt="logo" className={styles['header__logo']}/>
+            Fun Test
+        </NavLink>
+
+        {/* <div className={showMenu ? 'header__menu-show header__menu' : 'header__menu'} > */}
             {props.checkLogin ?
-                <ul className={styles['header__menu-navlinks']}>
-                    <li className={styles.header__item}><NavLink to='/view_profile'>view profile</NavLink></li>
-                    <li className={styles.header__item}><NavLink to='/ratings'>ratings</NavLink></li>
-                    <li className={styles.header__item}><NavLink to='/logout'>logout</NavLink></li>
+                <ul className={styles['header__menu-nav-links']}>
+                    <li className={styles.header__item}><NavLink className={styles['header__item-nav']} to='/view_profile'>view profile</NavLink></li>
+                    <li className={styles.header__item}><NavLink className={styles['header__item-nav']} to='/ratings'>ratings</NavLink></li>
+                    <li className={styles.header__item}><NavLink className={styles['header__item-nav']} to='/logout'>logout</NavLink></li>
                 </ul>
                 :
                 <div className={styles['header__menu-buttons']}>
-                    <button className={styles.header__button}>sign in</button>
+                    <button className={styles['header__button']+' '+styles['header__margine-right']}>sign in</button>
                     <button className={styles.header__button}>create an account</button>
                 </div>
             }
-        </div>
+            </div>
+        // </div>
     )
 };
 
